288314ff69612b36ef8066bc4935eb58dbdde137
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2021-02-06 19:05:52 +0100

    updated optimizee to read and write the parameter files for netlogo

--- l2l/optimizees/neuroevolution/optimizee.py
+++ l2l/optimizees/neuroevolution/optimizee.py
@@ -1,34 +1,44 @@
 import numpy as np
 import os
 import pandas as pd
+import time
 from collections import namedtuple
 from l2l.optimizees.optimizee import Optimizee
 
 NeuroEvolutionOptimizeeParameters = namedtuple(
-    'NeuroEvoOptimizeeParameters', ['path'])
+    'NeuroEvoOptimizeeParameters', ['path', 'seed'])
 
 
 class NeuroEvolutionOptimizee(Optimizee):
     def __init__(self, traj, parameters):
         super().__init__(traj)
-        self.rng = np.random.default_rng()
-        self.path = parameters.path
-        self.ind_idx=traj.individual.ind_idx
+        self.param_path = parameters.path
+        self.ind_idx = traj.individual.ind_idx
+        self.generation = traj.individual.generation
+        seed = parameters.seed * self.ind_idx + self.generation
+        self.rng = np.random.default_rng(seed)
+        self.dir_path = ''
 
     def create_individual(self):
         """
         Creates and returns the individual
 
-        Will launch netlogo and create a network instance to get the parameters.
-        Parameters are read in via a csv file and returned as a dictionary.
+        Creates the parameters for netlogo.
+        The parameter are `weights`, `plasticity` and `delays`.
         """
-        csv_path = os.path.join('.', 'individual_config_{}.csv'.format(self.ind_idx))
-        print(csv_path)
-        csv = pd.read_csv(csv_path)
+        # TODO the creation of the parameters should be more variable
+        #  e.g. as parameters or as a config file
+        # create random weights
+        weights = self.rng.uniform(-15, 15, 78)
+        # create plasticity {0,1} for on/off
+        plasticity = self.rng.integers(2, size=78)
+        # create delays
+        delays = self.rng.uniform(1, 7, 78)
+        # create individual
         individual = {
-            'weights': csv.iloc[0],
-            'plasticity': csv.iloc[1],
-            'delay': csv.iloc[2]
+            'weights': weights,
+            'plasticity': plasticity,
+            'delays': delays
         }
         return individual
 
@@ -36,13 +46,39 @@ class NeuroEvolutionOptimizee(Optimizee):
         """
         Simulate a run and return a fitness
 
-        Invokes a run of netlogo, reads in a file outputted by netlogo with
-        parameters inside to optimize.
+        A directory `individualN` and a csv file `individualN` with parameters
+        to optimize will be saved.
+        Invokes a run of netlogo, reads in a file outputted (`resultN`) by
+        netlogo with the fitness inside.
         """
+        weights = traj.weights
+        plasticity = traj.plasticity
+        delays = traj.delays
+        # create directory individualN
+        self.dir_path = os.path.join(self.param_path,
+                                     'individual{}'.format(self.ind_idx))
+        os.mkdir(self.dir_path)
+        individual = {
+            'weights': weights,
+            'plasticity': plasticity,
+            'delays': delays
+        }
+        # create the csv file and save it in the created directory
+        df = pd.DataFrame(individual)
+        df.to_csv(self.dir_path, 'individual{}'.format(self.ind_idx))
+        # call netlogo
+        # TODO adapt next line or make it a parameter for the bin file
+        #  or config file
+        os.system(
+            'bash /opt/netlogo/netlogo-headless.sh --model ~/Documents/toolbox/L2L/l2l/optimizees/neuroevolution/SpikingLab_Demo_Artificial_Insect_and_STDP_FlexTopology_L2L_FileParameters_27012021.nlogo --experiment experiment1 --table table1.csv')
+        file_path = os.path.join(self.dir_path, "result{}.csv".format(self.ind_idx))
+        while not os.path.isfile(file_path):
+            time.sleep(10)
+        # Read the results file after the netlogo run
         csv_path = os.path.join(
-            self.path, 'individual{}.csv'.format(traj.individual.ind_idx))
+            self.dir_path, 'result{}.csv'.format(traj.individual.ind_idx))
         csv = pd.read_csv(csv_path)
-        fitness = csv.iloc[3]
+        fitness = csv.iloc[0]
         return fitness
 
     def bounding_func(self, individual):
